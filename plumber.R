2954550bf1c29e3d89b73c2833e0df0cba5d4b75
Ramiro Steinmann Petrasso <ramirost@live.com>
2022-03-13 17:55:58 -0700

    Added stuff for reviews

--- plumber.R
+++ plumber.R
@@ -5,6 +5,9 @@ library(mongolite)
 connection_string = 'mongodb+srv://info448finalapp:info448finalapp@cluster0.x7en1.mongodb.net/finalApp?retryWrites=true&w=majority'
 classes_collection = mongo(collection="classes", db="finalApp", url=connection_string)
 reviews_collection = mongo(collection="reviews", db="finalApp", url=connection_string)
+comments_collection = mongo(collection="comments", db="finalApp", url=connection_string)
+isProfessor_collection = mongo(collection="isProfessor", 
+                               db="finalApp", url=connection_string)
 
 
 #* @apiTitle FinaLApp API
@@ -37,29 +40,73 @@ function(course) {
   as.data.frame(reviews_collection$find(paste0('{"course" : "', course,'" }')))
 }
 
-#* Returns countries that satisfy condition
+#* Returns all Comments from a specific Review
+#* @param course course the Review is for 
+#* @param author user who wrote review
+#* @get /allComentsForReview
+function(course, author) {
+  as.data.frame(comments_collection$find(paste0('{"reviewCourse":"', course, '", 
+                                                "reviewAuthor":"', author, '"}')))
+}
+
+#* Uploads review to the db
 #* @param course the class that this review is for 
 #* @param author user who wrote review
 #* @param numStars number of stars given in this review
 #* @param description what the user wrote
 #* @get /uploadReview
 function(course, author, numStars, description) {
-  val <- data.frame(course, author, numStars = strtoi(numStars, base=0L), description, stringsAsFactors=FALSE)
+  val <- data.frame(course, author, numStars = strtoi(numStars, base=0L), 
+                    description, stringsAsFactors=FALSE)
   
   reviews_collection$insert(val)
   
+  reviews_collection$update(paste0('{"course":"', course, '", "author":"', author, '"}'), 
+                            paste0('{"$set":{"numStars":', numStars, ', "description":"', 
+                                   description, '"}}'), upsert = TRUE)
   val
 }
-#* Returns countries that satisfy condition
+#* Uploads review to the db
 #* @param course the class that this review is for 
 #* @param author user who wrote review
 #* @param numStars number of stars given in this review
 #* @param description what the user wrote
 #* @post /uploadReview
 function(course, author, numStars, description) {
-  val <- data.frame(course, author, numStars = strtoi(numStars, base=0L), description, stringsAsFactors=FALSE)
+  val <- data.frame(course, author, numStars = strtoi(numStars, base=0L), 
+                    description, stringsAsFactors=FALSE)
   
-  reviews_collection$insert(val)
+  #reviews_collection$insert(val)
+  reviews_collection$update(paste0('{"course":"', course, '", "author":"', author, '"}'), 
+                            paste0('{"$set":{"numStars":', numStars, ', "description":"', 
+                                   description, '"}}'), upsert = TRUE)
+  val
+}
+
+#* Uploads comment to the db
+#* @param reviewCourse the class that the review is for 
+#* @param reviewAuthor user who wrote review
+#* @param commentAuthor User who wrote comment
+#* @param comment The comment the commenter made
+#* @get /makeComment
+function(reviewCourse,reviewAuthor, commentAuthor, comment) {
+  val <- data.frame(reviewCourse, reviewAuthor, commentAuthor, 
+                    comment, stringsAsFactors=FALSE)
   
+  comments_collection$insert(val)
+  val
+}
+
+#* Uploads comment to the db
+#* @param reviewCourse the class that the review is for 
+#* @param reviewAuthor user who wrote review
+#* @param commentAuthor User who wrote comment
+#* @param comment The comment the commenter made
+#* @post /makeComment
+function(reviewCourse,reviewAuthor, commentAuthor, comment) {
+  val <- data.frame(reviewCourse, reviewAuthor, commentAuthor, 
+                    comment, stringsAsFactors=FALSE)
+  
+  comments_collection$insert(val)
   val
 }
